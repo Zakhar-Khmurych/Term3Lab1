759d9947d0009c820775b9abf9a7b52351161a99
Zakhar-Khmurych <68910550+Zakhar-Khmurych@users.noreply.github.com>
2022-01-18 09:51:14 +0200

    Create attempt1.cs

---
+++ attempt1.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+
+namespace attempt1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string inputstuff = "2+43*0-1";
+            Console.WriteLine("Please insert your numbers. '+', '-', '*', '/', '^', '(', ')' are allowed");
+            //inputstuff = Console.ReadLine();
+            Console.WriteLine(inputstuff);
+        }
+    }
+}
